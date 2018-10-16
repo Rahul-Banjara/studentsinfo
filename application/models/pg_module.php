0406c2ce474ad0db3af0eb0412bfa6cd19cbb00d
Rahul-Banjara <banjaraindia1995@gmail.com>
2018-10-16 18:18:08 +0530

    success

--- application/models/pg_module.php
+++ application/models/pg_module.php
@@ -2,10 +2,10 @@
   
 class pgsql_module extends CI_Model {  
   
-   function fetchRecords(){    
+   /*function fetchRecords(){    
         $query = pg_query("select * from student");
         var_dump($query);
         return $query;
-    }
+    }*/
 }
 ?>  
